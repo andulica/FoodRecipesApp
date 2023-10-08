3bffcaff7b6c7e46c0ba969aee2b56671e28a53d
andulica <abalan91@yahoo.com>
2023-10-08 18:37:12 +0100

    Initial commit

---
+++ src/main/java/com/example/foodrecipesapp/FoodRecipesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodrecipesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecipesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRecipesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/foodrecipesapp/FoodRecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodrecipesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecipesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
