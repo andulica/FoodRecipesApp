650cea8f360817b0a4f2727e9d0fde0d32bed076
pavel <katiuwamutavchi@gmail.com>
2023-11-26 19:31:12 +0000

    was changes config to simple user with password for developers

--- src/main/java/com/example/foodrecipesapp/config/SecurityConfig.java
+++ src/main/java/com/example/foodrecipesapp/config/SecurityConfig.java
@@ -1,4 +1,20 @@
 package com.example.foodrecipesapp.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
 }
